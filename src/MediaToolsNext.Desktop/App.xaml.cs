6df5278f54b12856e50d8341b9b920f86d5125bb
Behrouz Talebi <behrouz@hazeltime.com>
2026-05-19 03:23:23 +0200

    Clean up Windows desktop branding metadata

--- src/MediaToolsNext.Desktop/App.xaml.cs
+++ src/MediaToolsNext.Desktop/App.xaml.cs
@@ -9,6 +9,6 @@ public partial class App : Application
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new MainPage()) { Title = "MediaToolsNext.Desktop" };
+		return new Window(new MainPage()) { Title = "Media Tools Next" };
 	}
 }
